569a5bea663ec633cc0b551fdb3a2652042efaef
trixxx.ortega <ortegatrici@gmail.com>
2025-04-09 20:53:27 -0300

    add is prime excercise

--- src/TryCatch.java
+++ src/TryCatch.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class TryCatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numero;
         while (true) {
             System.out.println("Por favor ingrese un número entero positivo mayor que cero: ");
 
             try {
-                int numero = scanner.nextInt();
-                if (numero >0) {
+                numero = scanner.nextInt();
+                if (numero > 0) {
                     System.out.println("El número ingresado es: " + numero);
                     break;
-                } else{
+                } else {
                     System.out.println("El numero ingresado no es un numero positivo.");
                 }
 
@@ -23,7 +23,28 @@ public class TryCatch {
             }
 
         }
-        scanner.close();
 
+        boolean isPrime = true;
+
+        if (numero == 1) {
+            System.out.println("El número ingresado " + numero + " no es un número primo");
+            scanner.close();
+            return;
+        } else {
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+
+            if (!isPrime) {
+                System.out.println("El número ingresado " + numero + " no es un número primo");
+            } else {
+                System.out.println("El número ingresado " + numero + " es un número primo");
+            }
+        }
+        scanner.close();
     }
 }
